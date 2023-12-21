86884b504c90b6a7cd48ab02c694a68fb8c8431a
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2023-12-21 14:38:14 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,7 +2,7 @@ package hello;
 
 import org.joda.time.LocalTime;
 
-public class HelloWorld {
+public class WELCOME TO INNODATA {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
